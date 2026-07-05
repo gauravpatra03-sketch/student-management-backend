d2cd823804cd3d2c2e77fa86c12917d40a045764
gauravpatra03-sketch <gaurav.patra03@gmail.com>
2026-07-05 13:59:57 +0530

    Change the front-end link in corsConfig.java

--- src/main/java/com/student/management/config/CorsConfig.java
+++ src/main/java/com/student/management/config/CorsConfig.java
@@ -17,7 +17,10 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins(
+                                "http://localhost:4200",
+                                "https://student-management-frontend-uk9k.onrender.com"
+                        )
                         .allowedMethods("*")
                         .allowedHeaders("*");
 
